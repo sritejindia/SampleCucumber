6745ef42ae8a4bed6e6b061b9a0b00b276658211
sritejindia <sritejindia@gmail.com>
2021-08-06 08:16:07 +0530

    New changes2

--- src/test/java/com/testdemo/RunCucumberTest1.java
+++ src/test/java/com/testdemo/RunCucumberTest1.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/report1.json"}, features = {"src/test/resources/com/testdemo/Sample.feature"} )
+@CucumberOptions(plugin = {"pretty", "json:target/report1.json"}, features = {"src/test/resources/com/testdemo/Sample.feature"}, strict = true )
 public class RunCucumberTest1 {
 }
--- src/test/java/com/testdemo/RunCucumberTest2.java
+++ src/test/java/com/testdemo/RunCucumberTest2.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/report2.json"}, features = {"src/test/resources/com/testdemo/Sample2.feature"} )
+@CucumberOptions(plugin = {"pretty", "json:target/report2.json"}, features = {"src/test/resources/com/testdemo/Sample2.feature"}, strict = true )
 public class RunCucumberTest2 {
 }
